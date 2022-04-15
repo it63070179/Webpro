32d4cc59d9339e01af6795200d8f450fcae1fb44
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-04-16 02:07:26 +0700

    Add files via upload

---
+++ views/ForgetPasswordPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>
+      <NavBar />
+    </div>
+
+    <p class="has-text-centered is-size-1 my-3">Forget Password</p>
+    <div class="columns is-mutiline">
+      <div class="column is-offset-4 is-4">
+      <p class="is-size-4 my-2">Username</p>
+      <input class="input my-2" type="text" placeholder="Text input">
+      <p class="is-size-4 my-2">Email Address</p>
+      <input class="input my-2" type="text" placeholder="Text input">
+      <p class="is-size-4 my-2">New Password</p>
+      <input class="input my-2" type="text" placeholder="Text input">
+      </div>
+    </div>
+    <div class="columns is-mutiline">
+      <div class="column is-offset-two-fifths is-2">
+      <button class="button is-medium is-fullwidth ml-5 mb-6">Reset Password</button>
+      </div>
+    </div>
+
+  </div>
+</template>
+<style>
+</style>
+
+<script>
+import NavBar from "../components/NavBar";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
