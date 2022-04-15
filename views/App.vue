fb3ba61aca5c14ef015e90c8ac261ee1dc75da26
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-04-15 22:01:54 +0700

    Create App.vue

---
+++ views/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="background-color: #f8dec7">
+    <div id="app">
+      <router-view></router-view>
+    </div>
+
+    <footer class="footer" style="background-color: #b08968">
+      <div class="columns">
+        <div class="column">
+          <div class="content has-text-left">
+            <p class="has-text-white">ติดต่อเรา</p>
+            <p class="has-text-white">02-9876-5432</p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content has-text-centered">
+            <p class="has-text-white">ติดต่อเรา</p>
+            <p class="has-text-white">02-9876-5432</p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content has-text-right">
+            <p class="has-text-white">ติดต่อเรา</p>
+            <p class="has-text-white">02-9876-5432</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
