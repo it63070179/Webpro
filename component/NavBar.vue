0e3b162309d9577dc7e610fbcfc8eacf4a43238c
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-04-15 21:24:50 +0700

    Create NavBar.vue

---
+++ component/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <b-navbar id="deletetop">
+        <template #start>
+          <b-navbar-item tag="div"
+            ><router-link to="/" style="color: white"> หน้าแรก </router-link></b-navbar-item
+          >
+          <b-navbar-item tag="div"
+            ><router-link to="/profile" style="color: white"> สินค้า </router-link></b-navbar-item
+          >
+          <b-navbar-item tag="div"><router-link to="/promotion" style="color: white"> โปรโมชัน </router-link></b-navbar-item>
+        </template>
+        <template #end>
+          <b-navbar-item tag="div" style="padding-top: 12px">
+            <div class="block" style="margin-bottom: 0">
+                 <i class="fas fa-shopping-cart"></i> 
+            </div>
+            <a class="button is-primary" style="margin-left: -2%">
+              <strong>สมัครสมาชิก</strong>
+            </a>
+            <a class="button is-light" style="margin-left: 4%">
+              <strong>เข้าสู่ระบบ</strong>
+            </a>
+          </b-navbar-item>
+        </template>
+      </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+    name: "NavBar"
+};
+</script>
