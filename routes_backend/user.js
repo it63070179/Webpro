8572f90af0e04e2f97c6e18140554372bde26d20
Apibun <73689795+Apibun@users.noreply.github.com>
2022-04-30 00:10:36 +0700

    Update user.js

--- routes_backend/user.js
+++ routes_backend/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const pool = require("../config");
 const multer = require("multer");
+const {
+    router
+} = require("./home");
 
 router = express.Router();
 
@@ -14,6 +17,30 @@ router.get("/user/:userid", async function (req, res, next) {
     }
 });
 
+router.post("/user/create", async function (req, res, next) {
+    // Begin transaction
+    const conn = await pool.getConnection();
+    await conn.beginTransaction();
+
+    try {
+        let adduser = await conn.query("INSERT INTO user(user_login, user_password, fname, lname, phone, email) " +
+            "VALUES(?, ?, ?, ?, ?, ?);",
+            [req.body.login, req.body.password, req.body.fname, req.body.lname, req.body.phone, req.body.email]
+        );
+        const adduserId = adduser[0].insertId;
+        console.log(adduser)
+        console.log(adduserId)
+        await conn.commit();
+        res.send("success!");
+    } catch (err) {
+        await conn.rollback();
+        return res.status(400).json(err);
+    } finally {
+        conn.release();
+    }
+
+});
+
 router.put("/user/:userid", async function (req, res, next) {
     const conn = await pool.getConnection()
     await conn.beginTransaction()
@@ -23,7 +50,7 @@ router.put("/user/:userid", async function (req, res, next) {
             [req.body.fname, req.body.lname, req.body.age, req.body.address, req.body.phone, req.body.email, req.params.userid])
         conn.commit()
         res.json({
-            message: "Update Blog id " + req.params.id + " Complete"
+            message: "Update user id " + req.params.userid + " Complete"
         })
     } catch (error) {
         await conn.rollback()
@@ -35,6 +62,25 @@ router.put("/user/:userid", async function (req, res, next) {
 
 });
 
+router.put("/user/forget/:userid", async function (req, res, next) {
+    const conn = await pool.getConnection()
+    await conn.beginTransaction()
 
+    try {
+        await conn.query('UPDATE user SET user_password WHERE id=?',
+            [req.body.password, req.params.userid])
+        conn.commit()
+        res.json({
+            message: "Update user id " + req.params.userid + " Complete"
+        })
+    } catch (error) {
+        await conn.rollback()
+        return next(error)
+    } finally {
+        console.log('finally')
+        conn.release()
+    }
+
+});
 
 exports.router = router;
