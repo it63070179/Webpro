cf933c06676feeb64ec812a3fed1a0bec36548d3
Apibun <73689795+Apibun@users.noreply.github.com>
2022-05-06 01:08:58 +0700

    Update CartPage.vue

--- views/CartPage.vue
+++ views/CartPage.vue
@@ -11,14 +11,14 @@
           style="background-color: #e7cdb7; position: relative"
         >
           <p style="margin-bottom: 10vh; margin-left: 38vw">สินค้า</p>
-          <article class="media">
+          <article class="media" v-for="cart in cart" :key="cart.product_id">
             <figure class="media-left">
-              <img />
+              <img :src="cart.image_path" />
             </figure>
             <div class="media-content">
               <div class="content" style="margin-left: 6vw; margin-top: 5vh">
                 <p>
-                  <strong>ชื่อสินค้า</strong>
+                  <strong>{{ cart.product_name }}</strong>
                   <br />
                 </p>
               </div>
@@ -40,20 +40,81 @@
               height: 2px;
             "
           />
-          <div style="margin-top: 15vh">
-            <span style="margin-bottom: 10vh; margin-left: 10vw">ราคา</span>
+          <div
+            style="margin-top: 20vh; padding-top: 5vh"
+            v-for="(cart, index) in cart"
+            :key="index"
+          >
+            <span style="margin-bottom: 10vh; margin-left: 10vw">{{
+              cart.product_price
+            }}</span>
             <span style="margin-bottom: 10vh; margin-left: 8.6vw">
-              <a><i class="fas fa-plus" style="color: #4a4a4a"></i></a>
-              <a class="is-size-6 mx-5" style="color: #4a4a4a">1</a>
-              <a><i class="fas fa-minus" style="color: #4a4a4a"></i></a
+              <a
+                ><i
+                  class="fas fa-minus"
+                  style="color: #4a4a4a"
+                  @click="reduceorderAmount(index)"
+                ></i
+              ></a>
+              <a class="is-size-6 mx-5" style="color: #4a4a4a">{{
+                cart.order_amount
+              }}</a>
+              <a
+                ><i
+                  class="fas fa-plus"
+                  style="color: #4a4a4a"
+                  @click="addorderAmount(index)"
+                ></i></a
             ></span>
-            <span style="margin-bottom: 10vh; margin-left: 8.8vw">ราคารวม</span>
+            <span style="margin-bottom: 10vh; margin-left: 8.8vw">{{
+              cart.order_amount * cart.product_price
+            }}</span>
             <i
               class="fas fa-trash-alt"
               style="margin-left: 2vw; font-size: 20px"
+              @click="ModalActive(index)"
             ></i>
+            <div class="modal" :class="{ 'is-active': isModalActive }">
+              <div class="modal-background"></div>
+              <div class="modal-card">
+                <header class="modal-card-head">
+                  <p class="modal-card-title"></p>
+                  <button
+                    class="delete"
+                    aria-label="close"
+                    @click="isModalActive = false"
+                  ></button>
+                </header>
+                <section class="modal-card-body">
+                  {{ textinModal }}
+                </section>
+                <footer class="modal-card-foot">
+                  <button class="button is-danger" @click="deleteProduct()">
+                    ใช่
+                  </button>
+                  <button class="button" @click="isModalActive = false">
+                    ยกเลิก
+                  </button>
+                </footer>
+              </div>
+            </div>
           </div>
         </div>
+        <div class="column is-3 is-offset-9 mt-2">
+          <p class="is-size-3">ราคารวม {{ sumPriceProduct }} ฿</p>
+        </div>
+      </div>
+      <div class="buttons has-addons is-right mr-5 mb-3 mt-1">
+        <button
+          class="button is-medium is-warning is-light has-text-grey-darker mr-6"
+          @click="OrderItem()"
+        >
+          <div>
+            <!-- <router-link :to="{ path: `/order/` + OrderNum }"> -->
+            สั่งซื้อสินค้า
+            <!-- </router-link> -->
+          </div>
+        </button>
       </div>
     </div>
   </div>
@@ -61,13 +122,91 @@
 
 <script>
 import NavBar from "../components/NavBar";
+import axios from "axios";
 export default {
-  data() {
-    return {};
-  },
-  methods: {},
   components: {
     NavBar,
+  },
+  data() {
+    return {
+      cart: [],
+      isModalActive: false,
+      textinModal: "",
+      sumPrice: 0,
+      delete: null,
+      OrderNum: 0,
+    };
+  },
+  mounted() {
+    console.log(localStorage.getItem("cart"));
+    this.checkCart();
+  },
+  methods: {
+    saveProduct() {
+      const cart = JSON.stringify(this.cart);
+      localStorage.setItem("cart", cart);
+    },
+    addorderAmount(index) {
+      this.cart[index].order_amount += 1;
+      console.log(this.cart[index]);
+      this.sumPrice = 0;
+      this.saveProduct();
+    },
+    reduceorderAmount(index) {
+      this.cart[index].order_amount -= 1;
+      console.log(this.cart[index]);
+      this.sumPrice = 0;
+      this.saveProduct();
+    },
+    ModalActive(index) {
+      console.log(index);
+      this.delete = index;
+      this.textinModal =
+        "คุณต้องการลบสินค้าชิ้นที่(" + Number(index + 1) + ")ใช่หรือไม่";
+      this.isModalActive = true;
+    },
+    deleteProduct() {
+      let del = this.cart.splice(this.delete, 1);
+      console.log(del);
+      this.sumPrice = 0;
+      this.saveProduct();
+      this.isModalActive = false;
+    },
+    OrderItem() {
+      axios
+        .post(`http://localhost:3000/order/${this.$route.params.id}`, {
+          cart: this.cart,
+          sumPrice: this.sumPrice,
+        })
+        .then((response) => {
+          this.OrderNum = response.data.orderId;
+          console.log(this.OrderNum);
+          this.$router.push({
+            path: `/order/${this.OrderNum}`,
+            params: { orderNum: this.OrderNum },
+          });
+        })
+        .catch((error) => {
+          this.error = error.response.data.message;
+        });
+    },
+    checkCart() {
+      let cartItem = JSON.parse(localStorage.getItem("cart"));
+      if (cartItem.length != 0) {
+        this.cart = cartItem;
+        console.log({ cartlocal: this.cart });
+      } else {
+        console.log("no item in cart");
+      }
+    },
+  },
+  computed: {
+    sumPriceProduct() {
+      this.cart.forEach((element) => {
+        this.sumPrice += element.product_price * element.order_amount;
+      });
+      return this.sumPrice;
+    },
   },
 };
 </script>
@@ -86,10 +225,17 @@ img {
   height: 200px;
   width: 300px;
   margin-left: 5vw;
-  /* background-color: #fff; */
+}
+
+.media + .media {
+  margin-top: 0;
 }
 
 .columns:last-child {
   margin-bottom: 0;
+}
+
+.modal-card-foot {
+  justify-content: flex-end;
 }
 </style>
