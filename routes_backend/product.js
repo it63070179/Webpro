b509da8c9840d5e64f9defbb1c01882924fbc005
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-05-06 06:58:46 +0700

    Update product.js

--- routes_backend/product.js
+++ routes_backend/product.js
@@ -171,32 +171,7 @@ router.post('/product/edit/:id', upload.array("myImage"), async function (req, r
 
 });
 
-router.post("/product/addpro", async function (req, res, next) {
-  const conn = await pool.getConnection()
-  await conn.beginTransaction();
 
-  const product_name = req.body.product_name
-  const product_desc = req.body.product_desc
-  const product_price = req.body.product_price
-  const product_type = req.body.product_type
-  const product_status = req.body.product_status
-  const product_amount = req.body.product_amount
-
-  try {
-    await conn.query(
-      'INSERT INTO product(product_name, product_desc, product_price, product_type, product_status, product_amount)' +
-      'VALUES (?, ?, ?, ?, ?, ?)',
-      [product_name, product_desc, product_price, product_type, product_status, product_amount]
-    )
-    conn.commit()
-    res.send()
-  } catch (err) {
-    conn.rollback()
-    res.status(400).json(err.toString());
-  } finally {
-    conn.release()
-  }
-});
 
 
 
