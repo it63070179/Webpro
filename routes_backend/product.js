901ee50ee393a7205e3e8dd5909f626c4dcbd94d
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-05-06 03:40:17 +0700

    Update product.js

--- routes_backend/product.js
+++ routes_backend/product.js
@@ -19,6 +19,15 @@ var storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+router.get("/product", async function (req, res, next) {
+  try {
+    const [allproduct, fields] = await pool.query(`SELECT * FROM product`);
+    return res.json(allproduct);
+  } catch (err) {
+    return res.status(500).json(err)
+  }
+});
+
 router.get("/product/bakery", async function (req, res, next) {
   try {
     let bakery = `SELECT * FROM product JOIN image USING (product_id) WHERE product_type = 'bakery' GROUP BY product_id`;
@@ -65,9 +74,9 @@ router.post("/product/add", upload.array("myImage"), async function (req, res, n
         'VALUES (?, ?, ?, ?, ?, ?)',
         [product_name, product_desc, product_price, product_type, product_status, product_amount],
       );
-      const productId = result[0].insertId; 
-      
-      req.files.forEach((file, index) => {
+      const productId = result[0].insertId;
+
+      req.files.forEach((file) => {
         let path = [productId, file.path.substring(6)];
         pathArray.push(path);
       });
@@ -89,47 +98,75 @@ router.post("/product/add", upload.array("myImage"), async function (req, res, n
 });
 
 router.get("/product/:id", function (req, res, next) {
-  
+
   const promise1 = pool.query("SELECT * FROM product WHERE product_id = ?", [req.params.id]);
   const promise2 = pool.query("SELECT * FROM image WHERE product_id = ?", [req.params.id]);
 
   Promise.all([promise1, promise2])
-      .then((results) => {
-          const [product, blogFields] = results[0];
-          const [images, imageFields] = results[1];
-          res.json({
-              products: product[0],
-              images: images,
-          });
-      })
-      .catch((err) => {
-          console.log(err)
-          return res.status(500).json(err);
+    .then((results) => {
+      const [product, blogFields] = results[0];
+      const [images, imageFields] = results[1];
+      res.json({
+        products: product[0],
+        images: images,
       });
-}); 
+    })
+    .catch((err) => {
+      console.log(err)
+      return res.status(500).json(err);
+    });
+});
 
-// router.delete('/product/:productId', async function (req, res, next) {
-//   const conn = await pool.getConnection()
-//   await conn.beginTransaction()
+router.put('/product/delete/:productId', async function (req, res, next) {
+  await pool.query('UPDATE product SET show_status = ? WHERE product_id = ?', ['hide', req.params.productId])
+  res.json()
+});
 
-//   try {
-//       await conn.query('DELETE FROM image WHERE product_id=?;', [req.params.productId])
-//       await conn.query('DELETE FROM promotion_product WHERE product_id=?;', [req.params.productId])
-//       await conn.query('DELETE FROM product_order_item WHERE product_id=?', [req.params.productId])
-//       await conn.query('DELETE FROM order_item WHERE product_id=?', [req.params.productId])
-//       await conn.query('DELETE FROM review WHERE product_id=?', [req.params.productId])
-//       await conn.query('DELETE FROM product WHERE product_id=?;', [req.params.productId])
-//       await conn.commit()
-//       res.json({
-//           message: 'Delete promotion id ' + req.params.productId + ' complete'
-//       })
-//   } catch (error) {
-//       await conn.rollback()
-//       next(error)
-//   } finally {
-//       console.log('finally')
-//       conn.release()
-//   }
-// });
+router.post('/product/edit/:id', upload.array("myImage"), async function (req, res, next) {
+    const file = req.files;
+    let pathArray = [];
+    if (!file) {
+      return res.status(400).json({ message: "Please upload a file" });
+    }
+    const conn = await pool.getConnection()
+    await conn.beginTransaction();
+
+    const product_name = req.body.product_name
+    const product_desc = req.body.product_desc
+    const product_price = req.body.product_price
+    const product_type = req.body.product_type
+    const product_status = req.body.product_status
+    const product_amount = req.body.product_amount
+
+    try {
+      await conn.query('UPDATE product SET product_name=? WHERE product_id=?', [product_name, req.params.id])
+      await conn.query('UPDATE product SET product_desc=? WHERE product_id=?', [product_desc, req.params.id])
+      await conn.query('UPDATE product SET product_price=? WHERE product_id=?', [product_price, req.params.id])
+      await conn.query('UPDATE product SET product_type=? WHERE product_id=?', [product_type, req.params.id])
+      await conn.query('UPDATE product SET product_status=? WHERE product_id=?', [product_status, req.params.id])
+      await conn.query('UPDATE product SET product_amount=? WHERE product_id=?', [product_amount, req.params.id])
+
+      const productId = req.params.id;
+
+      req.files.forEach((file) => {
+        let path = [productId, file.path.substring(6)];;
+        pathArray.push(path);
+      });
+
+      await conn.query('DELETE FROM image WHERE product_id=?', [req.params.id])
+      await conn.query('INSERT INTO image(product_id, image_path)' + 'VALUES ?',[pathArray])
+
+      conn.commit()
+      res.send()
+    } catch (err) {
+      conn.rollback()
+      res.status(400).json(err.toString());
+    } finally {
+      conn.release()
+    }
+
+});
+
+
 
 exports.router = router
