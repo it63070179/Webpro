3d589b2e0a699cf84e1e9ce330a48dc6e32b4e5a
it63070179 <73689108+it63070179@users.noreply.github.com>
2022-05-06 05:09:59 +0700

    Update order.js

--- routes_backend/order.js
+++ routes_backend/order.js
@@ -53,4 +53,37 @@ router.get("/order/:orderId", async function (req, res, next) {
 })
 
 
+// แสดง order ในหน้า manageorder
+router.get("/manageorder", async function (req, res, next) {
+  try {
+    const orders = await pool.query("SELECT * FROM `order`");
+    
+    res.send(orders[0])
+
+  }
+  catch (e) {
+    res.send(e)
+  }
+});
+
+// ถ้ากดปุ่ม yes จะขึ้นว่า sold แล้วหาย
+router.put('/yes/:id', async function (req, res, next) {
+  const [row, fields] = await pool.query(
+    'UPDATE `order` SET order_status = ? WHERE order_id = ?',
+    ['sold',req.params.id]
+  )
+  res.json()
+
+});
+
+// ถ้ากดปุ่ม cancel จะขึ้นว่า sold แล้วหาย
+router.put('/no/:id', async function (req, res, next) {
+  const [row, fields] = await pool.query(
+    'UPDATE `order` SET order_status = ? WHERE order_id = ?',
+    ['canceled',req.params.id]
+  )
+  res.json()
+
+});
+
 exports.router = router;
