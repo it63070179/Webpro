405523c0545aa1fd531e21550c0f43204eef99b8
Apibun <73689795+Apibun@users.noreply.github.com>
2022-05-06 09:52:11 +0700

    Update NavBar.vue

--- component/NavBar.vue
+++ component/NavBar.vue
@@ -21,7 +21,7 @@
       <template #end>
         <b-navbar-item tag="div" style="padding-top: 12px">
           <div class="block" style="margin-bottom: 0">
-            <router-link to="/cart">
+            <router-link :to="`/cart/${userId}`">
               <i
                 class="fas fa-shopping-cart"
                 style="margin-left: -60%; font-size: 30px; color: white"
@@ -48,18 +48,26 @@
               <strong>เข้าสู่ระบบ</strong>
             </router-link>
           </a>
-          <div class="navbar-item has-dropdown is-hoverable ml-2 mr-6 has-background-white" v-if="role" >
-            <a class="navbar-link"><i class="far fa-user" style="font-size: 30px;"></i> </a>
+          <div
+            class="
+              navbar-item
+              has-dropdown
+              is-hoverable
+              ml-2
+              mr-6
+              has-background-white
+            "
+            v-if="role"
+          >
+            <a class="navbar-link"
+              ><i class="far fa-user" style="font-size: 30px"></i>
+            </a>
 
             <div class="navbar-dropdown">
-              <router-link to="/profile"> 
-              <a class="navbar-item">
-                Profile
-              </a>
+              <router-link to="/profile">
+                <a class="navbar-item"> Profile </a>
               </router-link>
-              <a class="navbar-item" @click="logout">
-                Logout
-              </a>
+              <a class="navbar-item" @click="logout"> Logout </a>
             </div>
           </div>
         </b-navbar-item>
@@ -73,23 +81,65 @@
 }
 </style>
 <script>
+import axios from "axios";
 export default {
   name: "NavBar",
   data() {
     return {
       role: "",
+      userId: null,
     };
   },
   mounted() {
     // localStorage.getItem('role')
     this.role = localStorage.getItem("role");
-
+    this.getProfile();
+    this.userId = localStorage.getItem("userid");
   },
   methods: {
-    logout(){
-      localStorage.clear()
+    logout() {
+      localStorage.clear();
       this.$router.push({ name: "Login" });
-    }
-  }
+    },
+    getProfile() {
+      if (this.role == "employee") {
+        axios
+          .get(
+            `http://localhost:3000/profile/employee/` +
+              localStorage.getItem("userid")
+          )
+          .then((res) => {
+            console.log(res.data);
+            this.age = res.data.age;
+            this.lname = res.data.lname;
+            this.fname = res.data.fname;
+            this.phone = res.data.phone;
+            this.address = res.data.address;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        axios
+          .get(
+            `http://localhost:3000/profile/customer/` +
+              localStorage.getItem("userid")
+          )
+          .then((res) => {
+            console.log(res.data);
+            this.age = res.data.age;
+            this.lname = res.data.lname;
+            this.fname = res.data.fname;
+            this.phone = res.data.phone;
+            this.address = res.data.address;
+            this.email = res.data.email;
+            this.point = res.data.cus_point;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    },
+  },
 };
 </script>
